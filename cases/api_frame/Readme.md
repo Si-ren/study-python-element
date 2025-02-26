7f83b386c4939dd7c969ffed601344cbbfa1e0bd
Si-ren <wxr_624@163.com>
2025-02-26 14:47:28 +0800

    add interface test

--- cases/api_frame/Readme.md
+++
@@ -1,4 +0,0 @@
-# 使用说明
-```shell
-python.exe .\main.py -p .\cases\project_A
-```
--- cases/api_frame/main.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-import pytest
-import test_all_case
-
-if __name__ == '__main__':
-    pytest.main([])
